3f6822ebee4385604923085cfc9dff0407cadcd2
Charles Massry <charlesdmassry@gmail.com>
2015-02-28 01:13:20 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Charlie Massry on 2/28/15.
+//  Copyright (c) 2015 Charlie Massry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
