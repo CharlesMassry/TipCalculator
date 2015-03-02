12e57afba3f0d4e04a372cbffb15b12d2a791643
Charles Massry <charlesdmassry@gmail.com>
2015-03-01 23:19:29 -0500

    create basic tip calculation functionality

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -9,14 +9,39 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *totalAmount;
 @end
 
 @implementation ViewController
+- (IBAction)touchDecimal:(UIButton *)sender {
+    self.totalAmount.text = [self.totalAmount.text stringByAppendingString:@"."];
+}
+
+- (IBAction)touchNumber:(UIButton *)sender {
+    if ([self.totalAmount.text isEqualToString:@"0"]) {
+        self.totalAmount.text = @"";
+    }
+    NSUInteger integer = sender.currentTitle.integerValue;
+    NSString *integerString = [NSString stringWithFormat:@"%lu", integer];
+    self.totalAmount.text = [self.totalAmount.text stringByAppendingString:integerString];
+}
+
+- (IBAction)clearTouch:(UIButton *)sender {
+    self.totalAmount.text = @"0";
+}
+
+- (IBAction)percentageTouch:(UIButton *)sender {
+    CGFloat floatValue = sender.currentTitle.floatValue;
+    CGFloat percentage = floatValue / 100.0;
+    CGFloat total = [self.totalAmount.text floatValue];
+    CGFloat percentageAmount = total * percentage;
+    self.totalAmount.text = [NSString stringWithFormat:@"%.02f", percentageAmount];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.totalAmount.text = @"0";
 }
 
 - (void)didReceiveMemoryWarning {
